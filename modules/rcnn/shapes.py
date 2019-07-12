c0e4eac0e5d8bf5ccce0219be200d79c8fdc9c78
marsdenlab <gdmaher@stanford.edu>
2019-07-12 15:25:43 -0700

    shapes

--- modules/rcnn/shapes.py
+++ modules/rcnn/shapes.py
@@ -9,6 +9,7 @@ class Shape(object):
         raise RuntimeError("abstract not implemented")
 
 class Square(Shape):
+    shape_type = 0
     def paint(self, x, W, H):
         ox = int(self.box[0]*(W-1))
         oy = int(self.box[1]*(H-1))
@@ -20,6 +21,12 @@ class Square(Shape):
             x[oy:oy+h,ox:ox+w,i] = c
 
 class Circle(Shape):
+    shape_type = 1
+    def __init__(self, box, rgb):
+        self.box = box
+        self.box[2] = self.box[3]
+        self.rgb = rgb
+
     def paint(self, x, W, H):
         ox = int(self.box[0]*(W-1))
         oy = int(self.box[1]*(H-1))
@@ -34,10 +41,15 @@ class Circle(Shape):
 
         for i in range(oy,oy+h):
             for j in range(ox,ox+w):
-                if (cy-i)**2+(cx-j)**2 < r**2:
+                if (cy-i)**2+(cx-j)**2 < r**2 and i < H and j < W:
                     x[i,j,:] = self.rgb
 
 class Triangle(Shape):
+    shape_type = 2
+    def __init__(self, box, rgb):
+        self.box = box
+        self.box[2] = self.box[3]
+        self.rgb = rgb
     def paint(self, x, W, H):
         ox = int(self.box[0]*(W-1))
         oy = int(self.box[1]*(H-1))
@@ -48,3 +60,40 @@ class Triangle(Shape):
         for i,c in enumerate(self.rgb):
             for j in range(0,h):
                 x[oy+h-j, ox+j:ox+w-j,i] = c
+
+def get_random_shapes(n_min, n_max, H, W, shape_dist=[0.333,0.333,0.334]):
+    n_shapes = np.random.randint(n_min,n_max)
+
+    shapes = []
+    x = np.zeros((H,W,3))
+
+    types = [0,1,2]
+    hmax = 1.0/n_shapes
+
+    for i in range(n_shapes):
+        oy = (1.0-i*1.0/n_shapes)-hmax/2
+        ox = np.random.rand()*0.6+0.2
+        h  = np.random.rand()*(hmax-0.1)+0.1
+        w  = np.random.rand()*0.1+0.1
+        if ox+w > 1:
+            w = 0.99-ox
+        if oy+h > 1:
+            h = 0.99-oy
+            
+        box = np.array([ox,oy,w,h])
+
+        rgb = np.random.randint(255, size=3)
+
+        typ = np.random.choice(types, p=shape_dist)
+        print(box,typ)
+        if typ == 0:
+            s = Square(box,rgb)
+        elif typ == 1:
+            s = Circle(box,rgb)
+        else:
+            s = Triangle(box,rgb)
+
+        shapes.append(s)
+        s.paint(x, W, H)
+
+    return x,shapes
