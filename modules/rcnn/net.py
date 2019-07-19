90f0763fba90a3ac050895e9b43858d5a7bd9fd4
marsdenlab <gdmaher@stanford.edu>
2019-07-19 13:56:18 -0700

    rpn

--- modules/rcnn/net.py
+++ modules/rcnn/net.py
@@ -2,22 +2,22 @@ import tensorflow as tf
 import modules.layers_new as layers
 
 class Backbone(object):
-    def __init__(self, input_channels=3, output_size=256, scope='backbone'):
+    def __init__(self, input_channels=3, output_size=64, scope='backbone'):
         self.input_channels = input_channels
         self.output_size    = output_size
         self.scope          = scope
 
-        act = tf.contrib.keras.layers.LeakyReLU(0.2)
+        self.act = tf.contrib.keras.layers.LeakyReLU(0.2)
 
         with tf.variable_scope(scope):
             self.conv1 = layers.Conv2D(input_channels, nfilters=3*output_size,
-                init='xavier', activation=act, scope='conv1')
+                init='xavier', activation=self.act, scope='conv1')
 
             self.conv2 = layers.Conv2D(3*output_size, nfilters=2*output_size,
-                init='xavier', activation=act, scope='conv2')
+                init='xavier', activation=self.act, scope='conv2')
 
             self.conv3 = layers.Conv2D(2*output_size, nfilters=output_size,
-                init='xavier', activation=act, scope='conv3')
+                init='xavier', activation=self.act, scope='conv3')
 
     def __call__(self, x):
         o1 = self.conv1(x)
@@ -27,10 +27,34 @@ class Backbone(object):
         return o3
 
 class RPN(object):
-    def __init__(self):
-        pass
+    def __init__(self, backbone, backbone_channels=64, window_size=20, hidden_size=256, num_boxes=10, scope='rpn'):
+        self.backbone       = backbone
+        self.backbone_channels = backbone_channels
+        self.window_size    = window_size
+        self.hidden_size    = hidden_size
+        self.num_boxes      = num_boxes
+        self.scope          = scope
+
+        self.act = tf.contrib.keras.layers.LeakyReLU(0.2)
+
+        with tf.variable_scope(scope):
+            self.conv = layers.Conv2D(backbone_channels, dims=[window_size, window_size],
+                output_size=hidden_size, activation=self.act, scope='window_net')
+
+            self.object_conv = layers.Conv2D(hidden_size, dims=[1, 1],
+                output_size=num_boxes, activation=tf.nn.sigmoid, scope='object_conv')
+
+            self.obj_conv = layers.Conv2D(hidden_size, dims=[1, 1],
+                output_size=4*num_boxes, activation=tf.nn.sigmoid, scope='box_conv')
+
     def __call__(self, x):
-        pass
+        o1 = self.backbone(x)
+        o2 = self.conv(o1)
+
+        obj = self.object_conv(o2)
+        box = self.box_conv(o2)
+
+        return obj, box
 
 class RCNN(object):
     def __init__(self):
