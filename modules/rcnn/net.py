ef310d389678e6e2e599e312c1bc43064ab0160b
marsdenlab <gdmaher@stanford.edu>
2019-07-30 11:01:25 -0700

    bbox denormalization

--- modules/rcnn/net.py
+++ modules/rcnn/net.py
@@ -27,13 +27,18 @@ class Backbone(object):
         return o3
 
 class RPN(object):
-    def __init__(self, backbone, backbone_channels=64, window_size=20, hidden_size=256, num_boxes=9, scope='rpn'):
-        self.backbone       = backbone
+    def __init__(self, backbone, anchors, backbone_channels=64, window_size=20, hidden_size=256, scope='rpn'):
+        '''
+        anchors - 4xk array
+        '''
+        self.backbone          = backbone
         self.backbone_channels = backbone_channels
-        self.window_size    = window_size
-        self.hidden_size    = hidden_size
-        self.num_boxes      = num_boxes
-        self.scope          = scope
+        self.window_size       = window_size
+        self.hidden_size       = hidden_size
+        self.num_boxes         = anchors.shape[0]
+        self.anchors           = anchors
+        self.anchors_tensor    = tf.constant(anchors, dtype=tf.float32)
+        self.scope             = scope
 
         self.act = tf.contrib.keras.layers.LeakyReLU(0.2)
 
@@ -42,10 +47,12 @@ class RPN(object):
                 nfilters=hidden_size, activation=self.act, scope='window_net')
 
             self.object_conv = layers.Conv2D(hidden_size, dims=[1, 1],
-                nfilters=num_boxes, activation=tf.nn.sigmoid, scope='object_conv')
+                nfilters=self.num_boxes, activation=tf.nn.sigmoid, scope='object_conv')
 
             self.box_conv = layers.Conv2D(hidden_size, dims=[1, 1],
-                nfilters=4*num_boxes, activation=tf.nn.sigmoid, scope='box_conv')
+                nfilters=4*self.num_boxes, activation=tf.nn.sigmoid, scope='box_conv')
+
+            #anchor tensor?
 
     def __call__(self, x):
         o1 = self.backbone(x)
@@ -54,7 +61,26 @@ class RPN(object):
         obj = self.object_conv(o2)
         box = self.box_conv(o2)
 
+        s = box.get_shape().as_list()
+
+        shape = tf.constant([s[0],s[1],s[2],self.num_boxes,4])
+
+        box = tf.reshape(box, shape=shape)
+
         return obj, box
+
+    def get_box_tensor(self, x):
+        obj, box = self(x)
+        a = self.anchors_tensor
+
+        bx = box[:,:,:,:,0]*a[:,2]+a[:,0]
+        by = box[:,:,:,:,1]*a[:,3]+a[:,1]
+
+        bw = tf.exp(box[:,:,:,:,2])*a[:,2]
+        bh = tf.exp(box[:,:,:,:,3])*a[:,3]
+
+        out_boxes = tf.stack([bx,by,bw,bh], axis=4)
+        return out_boxes
 
 class RCNN(object):
     def __init__(self):
